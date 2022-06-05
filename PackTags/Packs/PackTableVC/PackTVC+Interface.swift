266ee0fe53b2855603a5d6b5e2b1ade19dfcb589
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2022-06-05 21:56:31 +0200

    update

--- PackTags/Packs/PackTableVC/PackTVC+Interface.swift
+++ PackTags/Packs/PackTableVC/PackTVC+Interface.swift
@@ -78,14 +78,19 @@ extension PackTableVC {
  }
 
 extension UIView {
-    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        
+        let path = UIBezierPath(
+            roundedRect: self.bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius))
+        
         let mask = CAShapeLayer()
         mask.path = path.cgPath
+        
         self.layer.mask = mask
     }
 }
-
 
 /*
 extension UIView{
